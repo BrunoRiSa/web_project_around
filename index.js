007706e3c6fd8067ad8093f918dbfe9877c55a43
Bruno Parada <brunosari96@gmail.com>
2024-03-17 17:07:41 -0300

    feat like btn action on js

--- index.js
+++ index.js
@@ -13,3 +13,29 @@ function popUpClose() {
 }
 
 closeBtn.addEventListener("click", popUpClose);
+
+let vtnc = document.querySelector(".vtnc");
+let btn = document.querySelector(".gallery__card-btn");
+let blck = document.querySelector(".gallery__card-btn-blck");
+
+var buttons = document.querySelectorAll(".gallery__card-btn");
+
+buttons.forEach(function (button) {
+  button.addEventListener("click", function () {
+    if (button.classList.contains("gallery__card-btn")) {
+      button.classList.remove("gallery__card-btn");
+      button.classList.add("gallery__card-btn-blck");
+    } else {
+      button.classList.add("gallery__card-btn");
+      button.classList.remove("gallery__card-btn-blck");
+    }
+  });
+});
+
+// function active() {
+//   vtnc.style.backgroundImage = "url(./images/vectors/blck-heart.svg)";
+// }
+
+// function inactive() {
+//   vtnc.style.backgroundImage = "url(./images/vectors/like-btn.svg";
+// }
